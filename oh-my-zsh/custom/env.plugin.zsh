2322a14cca3c2ef2cb8e49fc736021faf928f92c
Takayuki Sugita <sugilog@gmail.com>
2013-01-26 19:31:37 +0900

    use tmuxinator

--- oh-my-zsh/custom/env.plugin.zsh
+++ oh-my-zsh/custom/env.plugin.zsh
@@ -1,7 +1,11 @@
 export LESS=R
 export SVN_EDITOR=vim
+export EDITOR=vim
 
 alias mysql="mysql --auto-rehash"
 export MYSQL_PS1='\u@\h[\d]> '
 
 if [[ -s $HOME/.rvm/scripts/rvm ]] ; then source $HOME/.rvm/scripts/rvm ; fi
+
+# check tmuxinator env: tmuxinator doctor
+[[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
