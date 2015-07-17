d982a2ef2521ca784b59faac59a44392dd95481f
sugilog <sugilog@gmail.com>
2015-07-17 11:33:15 +0900

    add golang

--- setup.sh
+++ setup.sh
@@ -19,5 +19,5 @@ if [ ! -d $HOME/.oh-my-zsh ]; then
   ln -s $HOME/dotfiles/oh-my-zsh/custom/plugins/* $HOME/.oh-my-zsh/custom/plugins
   sed -i -e "1i export DISABLE_UPDATE_PROMPT=true" $HOME/.zshrc
   sed -i -e 's/ZSH_THEME="[^"]*"/ZSH_THEME="my_theme"/g' $HOME/.zshrc
-  sed -i -e "s/^plugins=(\(.*\))$/plugins=(\1 tmuxinator zshmarks)/g" $HOME/.zshrc
+  sed -i -e "s/^plugins=(\(.*\))$/plugins=(\1 tmuxinator zshmarks golang)/g" $HOME/.zshrc
 fi
