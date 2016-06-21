75923d5f6adde4671902dcec3e43e890713b10ef
sugilog <sugilog@gmail.com>
2016-06-21 11:35:35 +0900

    shで実行した時に`!#:3`が使えない。

--- setup/tools.sh
+++ setup/tools.sh
@@ -1,1 +1,2 @@
-curl https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight > $HOME/bin/diff-highlight && chmod 0755 !#:3
+_path=$HOME/bin/diff-highlight
+curl https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight > ${_path:?} && chmod 0755 ${_path:?}
