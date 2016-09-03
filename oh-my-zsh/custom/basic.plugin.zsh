bcec3619a711df7fc8f491a36d11d88b50c80194
sugilog <sugilog@gmail.com>
2016-09-03 12:59:29 +0900

    save last directory with zshmarks

--- oh-my-zsh/custom/basic.plugin.zsh
+++ oh-my-zsh/custom/basic.plugin.zsh
@@ -1,4 +1,10 @@
 function chpwd() {
-  # deletemark last
-  # bookmark last > /dev/null
+  local _markname="last"
+  local _directory=`showmarks ${_markname:?}`
+
+  if [[ " " != ${_directory:?} ]]; then
+    deletemark ${_markname:?}
+  fi
+
+  bookmark ${_markname:?} > /dev/null
 }
