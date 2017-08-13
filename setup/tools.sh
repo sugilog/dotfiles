a3a1967d2446c6a40871546e4acbc37c0badbb3b
sugilog <sugilog@gmail.com>
2017-08-13 21:31:19 +0900

    no need: diff-highlight

--- setup/tools.sh
+++ setup/tools.sh
@@ -1,4 +1,1 @@
 mkdir -p $HOME/bin
-
-_path=$HOME/bin/diff-highlight
-curl https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight/diff-highlight > ${_path:?} && chmod 0755 ${_path:?}
