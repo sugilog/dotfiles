3641c87a1d8ad83ff8d2378da9c8bdd391c1cb91
sugilog <sugilog@gmail.com>
2020-02-03 11:34:18 +0900

    brew's sbin path

--- oh-my-zsh/custom/env.plugin.zsh
+++ oh-my-zsh/custom/env.plugin.zsh
@@ -25,6 +25,7 @@ fi
 
 which brew > /dev/null 2>&1
 if [[ "$?" = "0" ]]; then
+  export PATH="/usr/local/sbin:$PATH"
   export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl)"
 fi
 
